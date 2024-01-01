dadeeff3b68ab06cbec1666d8f2a77f57441d8cd
takitajwar17 <tajwaruzzaman@iut-dhaka.edu>
2024-01-01 18:46:22 +0600

    Fixed Bugs

--- BSTVisualization.java
+++ BSTVisualization.java
@@ -1,7 +1,23 @@
 import java.util.*;
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class BSTVisualization extends JFrame implements ActionListener, KeyListener {
 	// Tree Root Node.
@@ -456,7 +472,7 @@ public class BSTVisualization extends JFrame implements ActionListener, KeyListe
 	}
 
 	public static void main(String[] arg) {
-		BSTVisualization bst = new BSTVisualization();
+		new BSTVisualization();
 
 	}
 }
